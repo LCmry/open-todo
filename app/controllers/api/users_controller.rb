743d750d774afaf9fa99b56eaa54b377e23ed371
LCmry <lcrees.2014@gmail.com>
2014-09-22 09:57:32 -0700

    Index test passing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,13 +4,13 @@ class Api::UsersController < Api::BaseController
 
   def index
     @users = User.all
-    respond_with @users.as_json(only: [:id, :username], root: true)
+    render json: @users
   end
 
   def create
     @user = User.new(params.permit(:username, :password))
     if @user.save
-      render json: @user.as_json(only: [:username, :password])
+      render json: @user
     else
       render json: {error: @user.errors.full_messages}, status: 422
     end
