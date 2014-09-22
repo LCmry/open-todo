27b5f33359f6b92a9b6ba1e252b8add1d047e3b0
LCmry <lcrees.2014@gmail.com>
2014-09-22 15:50:49 -0700

    Two passing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,7 +10,7 @@ class Api::UsersController < Api::BaseController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      render json: @user.as_json(only: [:username, :password])
     else
       render json: {error: @user.errors.full_messages}, status: 422
     end
