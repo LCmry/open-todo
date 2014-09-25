27e703dcb823b30c1bd0e1f8ecc11f4ea65f49bf
LCmry <lcrees.2014@gmail.com>
2014-09-24 20:52:21 -0700

    Fillin out spec and controller

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,6 +1,17 @@
 class Api::ListsController < Api::BaseController
 
+  def index
+    @user = User.find(params[:user_id])
+    if @user
+      @list = @user.lists
+    else
+      @list = List.find(params[permissions: 'visible'])
+    end
+    render json: @list
+  end
+
   def create
+    @user = User.find(params[:user_id])
     @list = List.new(list_params)
     if @list.save
       render json: @list.as_json(only: [:name, :permissions])
--- spec/controllers/api/lists_controller_spec.rb
+++ spec/controllers/api/lists_controller_spec.rb
@@ -4,6 +4,7 @@ describe Api::ListsController do
 
   before do
     List.destroy_all
+    User.destroy_all
   end
 
   describe "create" do
@@ -11,7 +12,7 @@ describe Api::ListsController do
       it "takes a list name, creates it if it doesn't exist, and returns false if it does" do
         user = create(:user)
         params = { 'list' => {'user_id' => user.id, 'name' => 'newlist', 'permissions' => 'private'} }
-
+        
         post :create, params
         response.should be_true
 
@@ -21,17 +22,48 @@ describe Api::ListsController do
     end
 
     context "without correct user's password" do
-      xit "it errors"
+      it "it errors" do
+        user = create(:user, password: 'wrongpass')
+        params = { 'list' => {'user_id' => user.id, 'name' => 'newlist', 'permissions' => 'private'} }
+
+        post :create, params
+        response.should be_false
+      end
     end
   end
 
   describe "index" do
     context "with correct user's password" do
-      xit "returns all lists associated with the user"
+      it "returns all lists associated with the user" do
+        user = create(:user)
+        list = create(:list)
+
+        get :index
+        JSON.parse(response.body).should ==
+        { 'lists' => 
+          [
+            { name: 'Shopping list', permissions: 'private' }
+          ]
+        }
+      end
     end
 
     context "without correct user's password" do
-      xit "returns all visible and open lists"
+      it "returns all visible and open lists" do
+        user = create(:user, password: 'wrongpass')
+        list = create(:list, user_id: user.id)
+        list1 = create(:list, name: 'Open list', permissions: 'open')
+        list2 = create(:list, name: 'Visible list', permissions: 'visible')
+
+        get :index
+        JSON.parse(response.body).should ==
+        { 'lists' =>
+          [
+            { name: 'Open list', permissions: 'open' },
+            { name: 'Visible list', permissions: 'visible' }
+          ]
+        }
+      end
     end
   end
 end
