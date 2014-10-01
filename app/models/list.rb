1215d4a3686a1c885785ee402de16e712faf87ea
LCmry <lcrees.2014@gmail.com>
2014-10-01 10:09:04 -0700

    Added permissions validator

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,7 @@ class List < ActiveRecord::Base
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :user_id
   validates :permissions, presence: true
+  validates_inclusion_of :permissions, in: %w(private viewable open), message: "not correct."
   validates :user_id, presence: true
 
   def self.permission_options
